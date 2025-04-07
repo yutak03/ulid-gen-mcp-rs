ae0a8fdbaf6c0d04109bffcb6606387dbe96c47e
yutak03 <yutak03.dev@gmail.com>
2025-04-07 17:08:41 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# ULID Generator MCP Server
+
+Model Context Protocol (MCP)を利用したULID（Universally Unique Lexicographically Sortable Identifier）を生成するためのサーバーです。
+
+[rmcp](https://github.com/modelcontextprotocol/rust-sdk)を利用しています。
+
+
+## 機能
+
+- MCPプロトコルを使用してULIDを生成
+
+## 必要条件
+
+- Rust 1.75以上
+- Cargo
+
+## インストール
+
+```bash
+git clone https://github.com/yutak03/ulid-gen-mcp-rs.git
+```
+
+## 使い方
+
+ビルドする。
+
+```bash
+cd ulid-gen-mcp-rs
+cargo build --release
+```
+
+パーミッションを実行できる形にしておく
+
+```bash
+chmod +x ./target/release/ulid_gen_server
+```
+
+### 設定ファイルを記載する
+
+
+```json
+{
+  "mcpServers": {
+    "ulid-gen-server": {
+      "command": "/path/to/ulid-gen-mcp-rs/target/release/ulid_gen_server",
+      "args": []
+    }
+  }
+}
+```
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use std::sync::{Arc, Mutex};
+
+use rmcp::{model::{CallToolResult, Content, Implementation, ProtocolVersion, ServerCapabilities, ServerInfo}, tool, ServerHandler, ServiceExt};
+use rmcp::transport::stdio;
+
+use tracing_subscriber::EnvFilter;
+
+#[derive(Debug, Clone)]
+struct Ulid {
+    ulid: Arc<Mutex<ulid::Ulid>>,
+}
+
+#[tool(tool_box)]
+impl Ulid {
+    fn new() -> Self {
+        Self {
+            ulid: Arc::new(Mutex::new(ulid::Ulid::new())),
+        }
+    }
+
+    #[tool(description = "generate a ULID")]
+    fn generate(&self) -> anyhow::Result<CallToolResult, rmcp::Error> {
+        let mut ulid = self.ulid.lock().unwrap();
+        *ulid = ulid::Ulid::new();
+        tracing::info!("Generated new ULID: {}", ulid);
+        Ok(CallToolResult::success(vec![
+            Content::text(ulid.to_string())
+            ])
+        )
+    }
+}
+
+#[tool(tool_box)]
+impl ServerHandler for Ulid {
+    fn get_info(&self) -> ServerInfo {
+        ServerInfo {
+            protocol_version: ProtocolVersion::V_2024_11_05,
+            capabilities: ServerCapabilities::builder()
+                .enable_prompts()
+                .enable_resources()
+                .enable_tools()
+                .build(),
+            server_info: Implementation::from_build_env(),
+            instructions: Some("This is a server for generating ULID".into()),
+
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+// Initialize the tracing subscriber with file and stdout logging
+    tracing_subscriber::fmt()
+    .with_env_filter(EnvFilter::from_default_env().add_directive(tracing::Level::DEBUG.into()))
+    .with_writer(std::io::stderr)
+    .with_ansi(false)
+    .init();
+
+    tracing::info!("Starting MCP server...");
+    
+    let service = Ulid::new().serve(stdio()).await.inspect_err(|e| {
+        tracing::error!("Error: {}", e);
+    }).unwrap_or_else(|_| {
+        std::process::exit(1);
+    });
+
+    service.waiting().await?;
+    Ok(())
+}
